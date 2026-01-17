b3eaab77de8a7137d2bf2bc878d416c9312c86fe
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-01-17 17:32:26 -0600

    Initial Commit

---
+++ Work Timer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Work Timer
+//
+//  Created by Ellie Tran on 1/17/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
